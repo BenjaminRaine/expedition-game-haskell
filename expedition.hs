d0d0fa25e9a3bb5bd85ae4ccb8fa9f869bc61791
brr2000 <brr2000@students.cs.ubc.ca>
2023-02-25 13:12:31 -0800

    Updates to Tree Definition and Some Functions
    I feel like today is gonna be a long day comprised entirely of this.

--- expedition.hs
+++ expedition.hs
@@ -1,30 +1,38 @@
--- CPSC 312 - 2023 - Twenty Questions in Haskell
---  Copyright David Poole 2023, released on GNU General Public License
-
 import System.IO
 
 -- String Option, String Result
 -- Int Required Resource, Int Required Resource Amount
 -- Int Resource Changed, Int Change Amount
 -- Story Tree options 1, 2, 3
-data StoryTree = StoryLeaf String String
-            | StoryNode String String Int Int Int Int StoryTree StoryTree StoryTree
+data StoryTree = StoryLeaf {option :: String,
+                            result :: String}
+               | StoryNode {option :: String,
+                            result :: String,
+                            reqresource :: Int,
+                            reqamount :: Int,
+                            changeresource :: Int,
+                            changeamount :: Int,
+                            option1 :: StoryTree,
+                            option2 :: StoryTree,
+                            option3 :: StoryTree}
 
 
 -- Basic loop of the game as we traverse the tree
 -- We print options, detect input, then traverse the tree 
-play :: StoryTree -> IO StoryTree
+play :: StoryTree -> StoryTree
 play tree =
    do
-      putStrLn "You have reached the top of Mount Everest, you now have 8 hours to leave the death zone."
+      printtreemessage tree
       printtreeoptions tree
       line <- getLineFixed
-      if (line `elem` ["1","2","3"])
+      if (line `elem` ["1","2","3"]) -- We need to go back to check these are actually met.
         then do
            newtree <- movedown tree line
            play newtree
         else return tree
 
+
+-- We move down to the selected option and
 movedown :: StoryTree -> String -> StoryTree
 movedown tree line =
     -- if line == "num" get that storyTree
@@ -32,19 +40,36 @@ movedown tree line =
     -- return the new option
     return tree
 
+printtreemessage:: StoryTree -> ()
+printtreemessage tree = putStrLn(result tree)
 
-printtreeoptions :: StoryTree -> Bool
+
+printtreeoptions :: StoryTree -> ()
 printtreeoptions tree =
-    -- Look at all 3 options,
-    -- Then print the option if the requirements are met
-    return true
+    printtreechoice(option1)
+    printtreechoice(option2)
+    printtreechoice(option3)
 
+
+printtreechoice :: StoryTree -> ()
+printtreechoice tree = putStrLn(option tree)
           
 -- Get player input
 getLineFixed =
    do
      line <- getLine
      return (fixdel line)
+
+
+-- Basic Nodes for Traversal Test (We should read from csv if there's time)
+
+
+
+
+main = do
+    print("THIS COMPILES")
+
+-- End of Basic Nodes -----------------------------------------------------
 
 
 -- ...
