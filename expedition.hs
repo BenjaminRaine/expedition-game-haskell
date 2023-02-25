1526e2db81cfa1a0ec9448e6357965da6cb01e6e
Benjamin Raine <65245523+BenjaminRaine@users.noreply.github.com>
2023-02-25 08:42:41 -0800

    Update expedition.hs

--- expedition.hs
+++ expedition.hs
@@ -1,71 +1,53 @@
 -- CPSC 312 - 2023 - Twenty Questions in Haskell
 --  Copyright David Poole 2023, released on GNU General Public License
 
-module TwentyQs where
-
 import System.IO
 
-data QATree = QLeaf String
-            | QNode String QATree QATree
-       deriving (Show)
+-- String Option, String Result
+-- Int Required Resource, Int Required Resource Amount
+-- Int Resource Changed, Int Change Amount
+-- Story Tree options 1, 2, 3
+data StoryTree = StoryLeaf String String
+            | StoryNode String String Int Int Int Int StoryTree StoryTree StoryTree
 
-initQATree = QNode "Is it living?"
-                (QNode "Is it a person?"
-                    (QLeaf "Justin Bieber")
-                    (QLeaf "J-35, one of the southern resident killer whales"))
-                (QNode "Is it a physical object?"
-                    (QLeaf "Vancouver")
-                    (QLeaf "CPSC 312"))
 
-play :: QATree -> IO QATree
+-- Basic loop of the game as we traverse the tree
+-- We print options, detect input, then traverse the tree 
+play :: StoryTree -> IO StoryTree
 play tree =
    do
-      putStrLn "Do you want to play 20 questions?"
-      ans <- getLineFixed
-      if (ans `elem` ["y","yes","ye","oui"])
+      putStrLn "You have reached the top of Mount Everest, you now have 8 hours to leave the death zone."
+      printtreeoptions tree
+      line <- getLineFixed
+      if (line `elem` ["1","2","3"])
         then do
-           putStrLn "Think of an entity"
-           newtree <- askabout tree
+           newtree <- movedown tree line
            play newtree
         else return tree
 
-askabout :: QATree -> IO QATree
-askabout (QLeaf ans) =
-  do
-    putStrLn("Is it "++ans++"?")
-    line <- getLineFixed
-    if (line `elem` ["y","yes","ye","oui"])
-       then return (QLeaf ans)
-       else do
-          putStrLn("What were you thinking of?")
-          obj <- getLineFixed
-          putStrLn("Give a question for which the answer is yes for "++obj++" and no for "++ans)
-          quest <- getLineFixed
-          return (QNode quest (QLeaf obj) (QLeaf ans))
-          
-askabout (QNode q yes no) =
-  do
-    putStrLn(q)
-    line <- getLineFixed
-    if (line `elem` ["y","yes","ye","oui"])
-       then do
-            newyes <- (askabout yes)
-            return (QNode q newyes no)
-       else do
-            newno <- askabout no
-            return (QNode q yes newno)
+movedown :: StoryTree -> String -> StoryTree
+movedown tree line =
+    -- if line == "num" get that storyTree
+    -- subtract the resource change
+    -- return the new option
+    return tree
 
+
+printtreeoptions :: StoryTree -> Bool
+printtreeoptions tree =
+    -- Look at all 3 options,
+    -- Then print the option if the requirements are met
+    return true
+
+          
+-- Get player input
 getLineFixed =
    do
      line <- getLine
-     return (fixdel2 line)
-     
-go :: IO QATree
-go = play initQATree
+     return (fixdel line)
 
------ Two Implementations fo fixdel ----
 
--- fixdel removes deleted elements from string
+-- ...
 fixdel st
    | '\DEL' `elem` st = fixdel (remdel st)
    | otherwise = st
@@ -73,25 +55,15 @@ remdel ('\DEL':r) = r
 remdel (a:'\DEL':r) = r
 remdel (a:r) = a: remdel r
 
--- fixdel2 deleted elements from string 
-fixdel2 :: [Char] -> [Char]
-fixdel2 st = fst (remdel2 st)
--- remdel2 st   returns (resulting_string, number_of_deletes_to_do)
-remdel2 :: [Char] -> ([Char], Int)
-remdel2 [] = ([],0)
-remdel2 ('\DEL':t) = (s,n+1)
-    where (s,n) = remdel2 t
-remdel2 (h:t)
-    | n>0 = (s,n-1)
-    | otherwise = (h:s,0)
-    where (s,n) = remdel2 t
 
+-- ...
 splitsep sep [] = [[]]
 splitsep sep (h:t)
     | sep h = []: splitsep sep t
     | otherwise = ((h:w):rest)
                 where w:rest = splitsep sep t
 
+-- ...
 readcsv filename =
   do
     file <- readFile filename
